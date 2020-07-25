ebd75016cdc33d4352b5f103881135aa5bed898c
deepend-dev <codespace@383c5f90e84b>
2020-07-25 18:46:20 +0000

    banner initiation

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setmovie] = useState([]);
@@ -13,15 +14,20 @@ function Banner() {
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
+      return request;
     }
     fetchData();
   }, []);
   return (
-    <header>
-      {}
-      {}
-      {}
-      {}
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      }}
+    >
+      <div className="banner__contents">
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+      </div>
     </header>
   );
 }
