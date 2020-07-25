2fe4de91c4e56dc9b69c248d625a56b4750c2343
deepend-dev <codespace@383c5f90e84b>
2020-07-25 15:59:19 +0000

    banner update

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
 function Banner() {
-  return <div></div>;
+  const [movie, setmovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setmovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+  return (
+    <header>
+      {}
+      {}
+      {}
+      {}
+    </header>
+  );
 }
 
 export default Banner;
