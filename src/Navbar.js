21775ebb7277ca5d690340c32a9c81afec85e7cd
Administrator <admin@example.com>
2020-07-26 13:45:42 +0530

    black background transition

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? handleShow(true) : handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="Navbar">
+    <div className={`navbar ${show && "navbar__black"}`}>
       <img
-        className="Navbar__logo"
+        className="navbar__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
         alt="Netflix logo"
+      />
+      <img
+        src="https://occ-0-4078-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABSLII-o6GmYPFo09Nlv7D5jVLJGKsBJnZFzFAj82yk-WfGl7mV_vbCPIK5h65iTgGTs1dobHjU5Nlwc0EwKaty5KYhoV.png?r=a41"
+        alt="Profile Avatar"
+        className="navbar__avatar"
       />
     </div>
   );
